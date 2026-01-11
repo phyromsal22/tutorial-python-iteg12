6eff2f66a0bfcdf5d7d08ce467943673e43911ce
phyromsal22 <tongtong123erty@gmail.com>
2026-01-11 22:59:50 +0700

    Add functions to calculate average for predefined sets of numbers

---
+++ funtion.py
@@ -0,0 +1,10 @@
+def average():
+    number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    total = 0
+    for num in number:
+        total = total + num
+    average = total / len(number)
+    return average
+
+
+print(average())
