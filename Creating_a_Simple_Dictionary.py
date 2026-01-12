82eb94e1d51037afb9dea2f126c33920ad365138
phyromsal22 <tongtong123erty@gmail.com>
2026-01-12 08:51:07 +0700

    Creating a Simple Dictionary

---
+++ Creating_a_Simple_Dictionary.py
@@ -0,0 +1,8 @@
+#A dictionry represents a phone book
+phone_book ={
+    "John": "123-456-7890",
+    "Jane": "987-654-3210",
+    "Bob": "555-555-5555"
+}
+phone_number = phone_book["Jane"]
+print("Jane's phone number is: " + phone_number)
