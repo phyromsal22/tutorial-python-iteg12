98613f6f0eb47986801414fb38c45fbb0c9c0afa
phyromsal22 <tongtong123erty@gmail.com>
2026-01-15 08:12:13 +0700

    infomation

---
+++ .vscode/infomation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// infomation 
+int main(){
+    printf("Name: SAL PHYROM\n");
+    printf("Age: 19\n");
+    printf("Hight School: Phnom Thom\n");
+    printf("Home Town: Banteay Meanchey\n");
+    printf("Nontact Number: 0883499541-014682441\n");
+    return 0;
+
+}
